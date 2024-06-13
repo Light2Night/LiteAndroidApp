4845285e211d01696198d52e80a1ae542c27f4ee
Приходько Владислав <3885vlad@gmail.com>
2024-06-13 14:13:12 +0300

    Added main page and top menu

--- app/src/main/java/com/it/liteapp/RegistrationActivity.java
+++ app/src/main/java/com/it/liteapp/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.it.liteapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,22 +9,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
-public class MainActivity extends BaseActivity {
+public class RegistrationActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_page), (v, insets) -> {
+        setContentView(R.layout.activity_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registration_page), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+
+        Glide.with(this)
+                .load("https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/corporate-user-icon.png")
+                .apply(new RequestOptions().override(400))
+                .into(imageView);
     }
 }
