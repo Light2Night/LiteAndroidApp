b3c01344f4f0d1e088d7dcfc1a1648f625a8af83
Приходько Владислав <3885vlad@gmail.com>
2024-06-16 16:21:40 +0300

    Fixed RegistrationFragment

--- app/src/main/java/com/it/liteapp/RegistrationFragment.java
+++ app/src/main/java/com/it/liteapp/RegistrationFragment.java
@@ -1,17 +1,58 @@
 package com.it.liteapp;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class RegistrationFragment extends Fragment {
+    private ImageView imageView;
+
+    private final ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    Uri imageUri = result.getData().getData();
+                    if (imageUri != null) {
+                        Glide.with(this).load(imageUri).into(imageView);
+                    }
+                }
+            }
+    );
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        imageView = view.findViewById(R.id.image);
+        Button button = view.findViewById(R.id.button);
+
+        Glide.with(this)
+                .load("https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/corporate-user-icon.png")
+                .apply(new RequestOptions().override(400))
+                .into(imageView);
+
+        button.setOnClickListener(v -> openImagePicker());
+
+        return view;
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        pickImageLauncher.launch(intent);
     }
 }
