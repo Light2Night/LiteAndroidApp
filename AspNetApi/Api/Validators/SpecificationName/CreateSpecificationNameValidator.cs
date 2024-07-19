1e07996c0754618735ce72a9655fb535dac6b193
Приходько Владислав <3885vlad@gmail.com>
2024-07-19 16:14:27 +0300

    Created validators

---
+++ AspNetApi/Api/Validators/SpecificationName/CreateSpecificationNameValidator.cs
@@ -0,0 +1,19 @@
+﻿using Api.Services.Interfaces;
+using Api.ViewModels.SpecificationName;
+using FluentValidation;
+
+namespace Api.Validators.SpecificationName;
+
+public class CreateSpecificationNameValidator : AbstractValidator<CreateSpecificationNameVm> {
+	public CreateSpecificationNameValidator(IExistingEntityCheckerService existingEntityCheckerService) {
+		RuleFor(sn => sn.Name)
+			.NotEmpty()
+				.WithMessage("Name is empty or null")
+			.MaximumLength(50)
+				.WithMessage("Name is too long");
+
+		RuleFor(sv => sv.CategoryId)
+			.MustAsync(existingEntityCheckerService.IsExistsCategoryIdAsync)
+				.WithMessage("Category with this id is not exists");
+	}
+}
---
+++ AspNetApi/Api/Validators/SpecificationName/UpdateSpecificationNameValidator.cs
@@ -0,0 +1,23 @@
+﻿using Api.Services.Interfaces;
+using Api.ViewModels.SpecificationName;
+using FluentValidation;
+
+namespace Api.Validators.SpecificationName;
+
+public class UpdateSpecificationNameValidator : AbstractValidator<UpdateSpecificationNameVm> {
+	public UpdateSpecificationNameValidator(IExistingEntityCheckerService existingEntityCheckerService) {
+		RuleFor(sn => sn.Id)
+			.MustAsync(existingEntityCheckerService.IsExistsSpecificationNameIdAsync)
+				.WithMessage("SpecificationName with this id is not exists");
+
+		RuleFor(sn => sn.Name)
+			.NotEmpty()
+				.WithMessage("Name is empty or null")
+			.MaximumLength(50)
+				.WithMessage("Name is too long");
+
+		RuleFor(sv => sv.CategoryId)
+			.MustAsync(existingEntityCheckerService.IsExistsCategoryIdAsync)
+				.WithMessage("Category with this id is not exists");
+	}
+}
